4e14e5caa625b23c4e6c1919b6ecbc7f8c491862
Maryam Anileleye <maryamanileleye.bincom@gmail.com>
2025-02-02 10:56:09 +0100

    Added koyeb.yml

--- stage0_API/stage0_API/wsgi.py
+++ stage0_API/stage0_API/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' stage0_API.stage0_API.settings') #stage0_API.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', ' stage0_API.settings') #stage0_API.
 
 application = get_wsgi_application()
