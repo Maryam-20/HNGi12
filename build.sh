cba974091cae8b76e0900213d910ff2515c4077a
Maryam Anileleye <maryamanileleye.bincom@gmail.com>
2025-02-02 12:13:34 +0100

    Changed the build and requirements directory

---
+++ build.sh
@@ -0,0 +1,19 @@
+ECHO is on.
+#!/usr/bin/env bash
+# Exit on error
+chmod +x build.sh
+
+set -o errexit
+
+# Upgrade pip, setuptools, and wheel
+pip install --upgrade pip setuptools wheel
+
+# Install dependencies
+# pip install -r requirements.txt
+
+pip install --no-cache-dir --force-reinstall -r requirements.txt
+
+# Run database migrations
+python manage.py migrate --noinput
+
+echo "Build complete!"
