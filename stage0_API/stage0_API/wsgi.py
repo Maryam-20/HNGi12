c45889fc856694b88e20185f4fb3e8c009554da1
Maryam Anileleye <maryamanileleye.bincom@gmail.com>
2025-02-02 13:55:46 +0100

    Fix wsgi module import error

--- stage0_API/stage0_API/wsgi.py
+++ stage0_API/stage0_API/wsgi.py
@@ -39,7 +39,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 # Set the correct settings module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stage0_API.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE','stage0_API.settings')
 
 application = get_wsgi_application()
 
